e3b33cee2539becb679a8438b393990bd8f69147
Mate Lakat <mate.lakat@citrix.com>
2013-01-08 15:53:52 +0000

    xenapi: Refactor: Remove dead code, renames
    Related to blueprint xenapi-volume-drivers
    
    Remove dead sm code, and make internal volumeops methods start with
    underscore.
    
    Change-Id: Ie5457c3d7823ed77f7ad90252d0c16558e8cfb4f

--- nova/tests/virt/xenapi/test_volumeops.py
+++ nova/tests/virt/xenapi/test_volumeops.py
@@ -22,7 +22,7 @@ from nova.virt.xenapi import volumeops
 class VolumeAttachTestCase(test.TestCase):
     def test_attach_volume_call(self):
         ops = volumeops.VolumeOps('session')
-        self.mox.StubOutWithMock(ops, 'connect_volume')
+        self.mox.StubOutWithMock(ops, '_connect_volume')
         self.mox.StubOutWithMock(volumeops.vm_utils, 'vm_ref_or_raise')
         self.mox.StubOutWithMock(volumeops.volume_utils, 'get_device_number')
 
@@ -32,7 +32,7 @@ class VolumeAttachTestCase(test.TestCase):
         volumeops.volume_utils.get_device_number('mountpoint').AndReturn(
             'devnumber')
 
-        ops.connect_volume(
+        ops._connect_volume(
             'conn_data', 'devnumber', 'instance_1', 'vmref', hotplug=True)
 
         self.mox.ReplayAll()
@@ -42,7 +42,7 @@ class VolumeAttachTestCase(test.TestCase):
 
     def test_attach_volume_no_hotplug(self):
         ops = volumeops.VolumeOps('session')
-        self.mox.StubOutWithMock(ops, 'connect_volume')
+        self.mox.StubOutWithMock(ops, '_connect_volume')
         self.mox.StubOutWithMock(volumeops.vm_utils, 'vm_ref_or_raise')
         self.mox.StubOutWithMock(volumeops.volume_utils, 'get_device_number')
 
@@ -52,7 +52,7 @@ class VolumeAttachTestCase(test.TestCase):
         volumeops.volume_utils.get_device_number('mountpoint').AndReturn(
             'devnumber')
 
-        ops.connect_volume(
+        ops._connect_volume(
             'conn_data', 'devnumber', 'instance_1', 'vmref', hotplug=False)
 
         self.mox.ReplayAll()
@@ -85,7 +85,7 @@ class VolumeAttachTestCase(test.TestCase):
         self.stubs.Set(ops._session, 'call_xenapi', fake_call_xenapi)
 
         self.mox.StubOutWithMock(volumeops.volume_utils, 'parse_sr_info')
-        self.mox.StubOutWithMock(ops, 'introduce_sr')
+        self.mox.StubOutWithMock(ops, '_introduce_sr')
         self.mox.StubOutWithMock(volumeops.volume_utils, 'introduce_vdi')
         self.mox.StubOutWithMock(volumeops.vm_utils, 'create_vbd')
 
@@ -93,7 +93,7 @@ class VolumeAttachTestCase(test.TestCase):
             connection_data, sr_label).AndReturn(
                 tuple([sr_uuid, sr_label, sr_params]))
 
-        ops.introduce_sr(sr_uuid, sr_label, sr_params).AndReturn(sr_ref)
+        ops._introduce_sr(sr_uuid, sr_label, sr_params).AndReturn(sr_ref)
 
         volumeops.volume_utils.introduce_vdi(
             session, sr_ref, vdi_uuid, None).AndReturn(vdi_ref)
@@ -104,7 +104,7 @@ class VolumeAttachTestCase(test.TestCase):
 
         self.mox.ReplayAll()
 
-        ops.connect_volume(connection_data, dev_number, instance_name,
+        ops._connect_volume(connection_data, dev_number, instance_name,
                            vm_ref, hotplug=False)
 
         self.assertEquals(False, called['xenapi'])
